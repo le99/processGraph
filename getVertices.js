f99a93a162d7d166f851a8e97fd08e3618e9987d
l <le.florez602@uniandes.edu.co>
2019-05-12 17:50:49 -0500

    Better getVertices implementation

--- getVertices.js
+++ getVertices.js
@@ -10,11 +10,11 @@ const stream = require('stream');
 
 var pass = new stream.PassThrough({objectMode: true});
 
-// const IN_FILE = './data/in/XMLs/map.xml';
-// const OUT_FILE = './data/out/XMLs/mapVertices.json';
+const IN_FILE = './data/in/XMLs/map.xml';
+const OUT_FILE = './data/out/XMLs/mapVertices.json';
 
-const IN_FILE = './data/in/XMLs/Central-WashingtonDC-OpenStreetMap.xml';
-const OUT_FILE = './data/out/XMLs/CentralMapVertices.json';
+// const IN_FILE = './data/in/XMLs/Central-WashingtonDC-OpenStreetMap.xml';
+// const OUT_FILE = './data/out/XMLs/CentralMapVertices.json';
 
 // const IN_FILE = './data/exampleMap.xml';
 // const OUT_FILE = './data/out/exampleMapVertices.json';
@@ -23,50 +23,54 @@ var progress = 0;
 var t0 = new Date().getTime();
 
 var file = fs.createReadStream(IN_FILE);
-var out = fs.createWriteStream(OUT_FILE)
+var out = fs.createWriteStream(OUT_FILE);
 
-var xml = new XmlStream(file);
-var firstLine = true;
+var saxStream = sax.createStream(true, {})
 
-// xml.preserve('osm > node', true);
-xml.on('endElement: osm > node', function(item) {
-  // pass.write({id: item.$.id, lat: item.$.lat, lon: item.$.lon});
-  // pass.resume();
-  if(!firstLine){
-    out.write('\r\n');
+var firstLine = false;
+var openNode = false;
+saxStream.on("error", function (e) {
+  // unhandled errors will throw, since this is a proper node
+  // event emitter.
+  console.error("error!", e)
+  // clear the error
+  this._parser.error = null
+  this._parser.resume()
+})
+saxStream.on("opentag", function (node) {
+  // console.log(node);
+  if(node.name === 'node'){
+    openNode = true;
+    if(!firstLine){
+      out.write('\r\n');
+    }
+    else{
+       firstLine = false;
+     }
+    out.write(JSON.stringify({
+      id: node.attributes.id,
+      lat: node.attributes.lat,
+      lon: node.attributes.lon
+    }));
   }
-  else{
-    firstLine = false;
-  }
-  out.write(JSON.stringify({id: item.$.id, lat: item.$.lat, lon: item.$.lon}));
-
-  progress++;
-  if(progress % 10000 === 0){
-    console.log(progress);
-  }
-  
 });
-xml.on('end', function(){
-  // pass.end();
+
+saxStream.on("closetag", function (node) {
+  if(node === 'node'){
+    openNode = false;
+
+    progress++;
+    if(progress % 50000 === 0){
+      console.log(progress);
+    }
+  }
+
+});
+
+saxStream.on('end', function () {
   out.end();
   console.log('time:' + ((new Date().getTime() - t0)/1000));
 });
 
-// pass
-//   .pipe(es.map(function (data, cb) { //turn this async function into a stream
-//       // console.log('data');
-//       // console.log( data);
-//       cb(null, data);   //render it nicely
-//       // progress++;
-//       // if(progress % 10000 === 0){
-//       //   console.log(progress);
-//       // }
-//     }))
-//   .pipe(JSONStream.stringify())
-//   .pipe(out)
-//   .on('error', function(err){
-//     console.log(err);
-//   })
-//   .on('finish', function(){
-//     console.log('time:' + ((new Date().getTime() - t0)/1000));
-//   });
+file
+  .pipe(saxStream);
